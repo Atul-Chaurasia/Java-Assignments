0585a41d87222327d50a8d29e837468bfb541644
Atul-Chaurasia <89525470+Atul-Chaurasia@users.noreply.github.com>
2021-10-22 20:51:25 +0530

    Assignment Day 4

---
+++ Day4.java
@@ -0,0 +1,13 @@
+class Day4{
+
+    public static void main(String[] args){
+
+        // System.out.println("Hii "+ args[0] + " " + args[1]);
+
+        int x = Integer.parseInt(args[0]);
+        int y = Integer.parseInt(args[1]);
+
+        int z = x+y;
+        System.out.println("The Sum of "+x+" and "+y+" is : "+z);
+    }
+}
